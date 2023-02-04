714e2165532af458fd73eb3df62fa8002b617d8d
Luna Haines <luna.haines@icloud.com>
2023-02-03 17:46:05 -0700

    Added colors to overall image and added missing teams

--- hubris.py
+++ hubris.py
@@ -114,8 +114,50 @@ teams = [
         "name": "Core Mechanics",
         "primary color": "#ff430a",
         "secondary color": "#858585"
+    },
+    {
+        "name": "Atlantis Georgias",
+        "primary color": "#00dbc2",
+        "secondary color": "#5cffec"
+    },
+    {
+        "name": "Ohio Worms",
+        "primary color": "#5c4822",
+        "secondary color": "#ba9c65"
+    },
+    {
+        "name": "Houston Spies",
+        "primary color": "#67556b",
+        "secondary color": "#9e82a4"
     }
 ]
+
+custom_palette = {
+    "Baltimore Crabs": "#593037",
+    "Boston Flowers": "#d791e3",
+    "Broken Ridge Jazz Hands": "#6388ad",
+    "Seattle Garages": "#2606d7",
+    "Canada Moist Talkers": "#ed1c24",
+    "Charleston Shoe Thieves": "#ffce0a",
+    "LA Unlimited Tacos": "#9822d3",
+    "Miami Dale": "#bf00ff",
+    "Dallas Steaks": "#8c8d8f",
+    "Hades Tigers": "#5c1c1c",
+    "Mexico City Wild Wings": "#693f26",
+    "Philly Pies": "#008080",
+    "Hawai'i Fridays": "#3ee652",
+    "San Francisco Lovers": "#f193b3",
+    "Kansas City Breath Mints": "#178f55",
+    "Chicago Firefighters": "#8c2a3e",
+    "Yellowstone Magic": "#fcb040",
+    "New York Millennials": "#f8d6d7",
+    "Moab Hellmouth Sunbeams": "#fffbab",
+    "Tokyo Lift": "#e536c8",
+    "Core Mechanics": "#ff430a",
+    "Atlantis Georgias": "#00dbc2",
+    "Ohio Worms": "#5c4822",
+    "Houston Spies": "#67556b"
+}
 
 df.drop([24, 25, 26, 27, 28, 29], inplace=True)
 df.drop(columns=["Unnamed: 0", "Unnamed: 1"], inplace=True)
@@ -133,14 +175,15 @@ melted = temp.melt(id_vars="Name")
 melted = melted.rename(columns={"value": "Rankings"})
 
 # Overall Plot
-axis = sns.boxplot(data=melted, y="Rankings", x="Name", orient="v", palette="muted", boxprops={"linewidth": 0.5}, medianprops={"linewidth": 0.8})
+axis = sns.boxplot(data=melted, y="Rankings", x="Name", hue="Name", orient="v", palette=custom_palette, dodge=False, boxprops={"linewidth": 0.5}, medianprops={"linewidth": 0.8})
 axis.tick_params(axis="x", labelrotation=30)
 plt.xticks(ha="right")
 plt.subplots_adjust(bottom=0.3, left=0.2)
 axis.set(xlabel=None)
+axis.get_legend().remove()
 fig = axis.get_figure()
 fig.set_figwidth(10)
-fig.savefig("output/overall.png", transparent=True)
+fig.savefig("output/overall.png", transparent=False)
 
 # reset plot in between runs
 plt.clf()
@@ -159,5 +202,5 @@ for t in teams:
     axis.set(xlabel=None)
 
     fig = axis.get_figure()
-    fig.savefig(f"output/{name}.png", transparent=True)
+    fig.savefig(f"output/{name}.png", transparent=False)
     plt.clf()
